00f5330dbc1a3aae6c27e8d2f5a2ed7676da5b3b
Julie <julie.castaingmolin@gmail.com>
2026-03-24 16:21:02 +0100

    je comprends rien

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,64 +1,86 @@
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using UnityEditor.Overlays;
+using UnityEngine;
 
-public static class Timer
+public class Timer
 {
-    private static readonly Stopwatch stopwatch = new();
-    private static readonly List<long> steps = new();
+	private static readonly Stopwatch stopwatch = new();
+	private static  List<long> steps = new();
 
-    public static bool IsRunning
-    {
-        get => stopwatch.IsRunning;
-    }
+	private List<long> previousSteps = new();
 
-    public static double ElapsedSeconds
-    {
-        get => stopwatch.ElapsedMilliseconds * 0.001f;
-    }
+	public static bool IsRunning
+	{
+		get => stopwatch.IsRunning;
+	}
 
-    public static int StepsCount
-    {
-        get => steps.Count;
-    }
+	public static double ElapsedSeconds
+	{
+		get => stopwatch.ElapsedMilliseconds * 0.001f;
+	}
 
-    public static double GetStepElapsedSeconds(int index)
-    {
-        return steps[index] * 0.001f;
-    }
+	public static int StepsCount
+	{
+		get => steps.Count;
+	}
 
-    /// <summary>
-    /// Reset the timer and remove any steps.
-    /// </summary>
-    public static void Reset()
-    {
-        stopwatch.Reset();
-        steps.Clear();
-    }
+	public static double GetStepElapsedSeconds(int index)
+	{
+		return steps[index] * 0.001f;
+	}
 
-    public static void Start()
-    {
-        stopwatch.Start();
-    }
+	/// <summary>
+	/// Reset the timer and remove any steps.
+	/// </summary>
+	public static void Reset()
+	{
+		stopwatch.Reset();
+		steps.Clear();
+	}
 
-    public static void Stop()
-    {
-        stopwatch.Stop();
-    }
+	public static void Start()
+	{
+		stopwatch.Start();
+	}
 
-    public static void Step()
-    {
-        steps.Add(stopwatch.ElapsedMilliseconds);
-    }
+	public static void Stop()
+	{
+		stopwatch.Stop();
+	}
 
-    public static void Save()
-    {
-        // TODO : save our time steps (line 7 of this script) inside a file.
-    }
+	public static void Step()
+	{
+		steps.Add(stopwatch.ElapsedMilliseconds);
+	}
 
-    public static void Load()
-    {
-        // TODO : load our time steps from a file (if we have any)
-        // and store them inside our steps variable (line 7 of this script)
-        // to show them to the player before starting a race.
-    }
+	public static void Save()
+	{
+		// TODO : save our time steps (line 7 of this script) inside a file.
+		Timer data = new Timer();
+		//data.steps = steps;
+
+		string json = JsonUtility.ToJson(data, true);
+		string path = Application.persistentDataPath + "/Score.json";
+		System.IO.File.WriteAllText( path, json );
+	}
+
+	public static void Load()
+	{
+		// TODO : load our time steps from a file (if we have any)
+		// and store them inside our steps variable (line 7 of this script)
+		// to show them to the player before starting a race.
+		string path = Application.persistentDataPath + "/Score.json";
+		if (File.Exists(path))
+		{
+			string json = System.IO.File.ReadAllText(path);
+			Timer loadedData = JsonUtility.FromJson<Timer>(json);
+			//List<long> loadedListe = new List<long>(data.steps);
+
+			//Timer.List.previousSteps = loadedListe;
+		}
+
+		
+	}
 }
